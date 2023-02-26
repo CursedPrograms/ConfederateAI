56d9dcddbaac4f561c740983e1f1731a3702bde3
Cursed <110344485+CursedPrograms@users.noreply.github.com>
2023-02-26 13:52:06 +0200

    Update and rename AIControllerCore.cs to ControllerCore/AIControllerCore.cs

---
+++ ControllerCore/AIControllerCore.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[AddComponentMenu("Confederate AI/AIController")]
+[DisallowMultipleComponent]
+[RequireComponent(typeof(AIObjects))]
+[RequireComponent(typeof(AIUniversalVariables))]
+[RequireComponent(typeof(AIWaypoints))]
+[RequireComponent(typeof(GlobalTracker))]
+public class AIControllerCore : MonoBehaviour
+{
+    //This goes on your GameController
+}
+
