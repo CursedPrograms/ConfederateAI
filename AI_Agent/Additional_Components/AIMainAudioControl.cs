503615270c7371a5db16c0c1eb35db1fb644cdc3
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2023-09-25 16:42:30 +0200

    Add files via upload

---
+++ AI_Agent/Additional_Components/AIMainAudioControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class AIMainAudioControl : MonoBehaviour
+{
+    public AIAudioControl AIaudio;
+    chooseAudioArray aaiAudio;
+
+    void Start()
+    {
+    AIaudio = GetComponentInChildren<AIAudioControl>();
+    }
+
+    public void TargetHit()
+    { if (AIaudio.canPlayAudio)
+        {
+            
+        }
+    }
+}
